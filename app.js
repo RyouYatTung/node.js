6ac5fd8d8300aaad483d69e9affd06ccc084f16c
RyouYatTung <leungyattung1108@gmail.com>
2023-11-28 23:26:24 +0900

    push

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  if (url === "/") {
-    res.write(`
-    <html>
-      <head>
-        <title>enter message</title>
-      </head>
-      <body>
-        <h1>ddd</h1>
-      </body>
-    </html>
-  `);
-  }
+function rqListener(req, res) {}
 
-  res.setHeader("Content-Type", "text/html");
-  res.write(`
-    <html>
-      <head>
-        <title>ht¥eeee</title>
-      </head>
-      <body>
-        <h1>ddd</h1>
-      </body>
-    </html>
-  `);
-  res.end();
-});
-
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+http.createServer(rqListener);
